28d1a5d0549e4d30997aa2acd85099c545317266
Dineshragipani <dineshragipai143@gmail.com>
2025-03-18 13:30:44 +0530

    react App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 //import logo from './logo.svg';
 import './App.css';
 import Register  from './components/Register';
+import Products from 'Products';
+import Footer from 'Footer';
+import {BrowserRouter,Route,Routes} from "react-router-dom";
 function App(){
   return (
     <div>
-      <Register />
-      <cart />
-      <login />
+      <BrowserRouter>
+        <Header />
+        <Products />
+        <Footer />
+        <Routes>
+          <Route index element={<Products />} ></Route>
+          <Route path="/products" element={<Products />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
